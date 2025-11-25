367f71b4b1def41b51a722b1bc8dd9a9d96a50ad
Rohit Dixit <rd250729@gmail.com>
2025-11-25 12:18:26 +0530

    grid setup configuration

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,11 +4,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -16,6 +19,7 @@ import org.testng.annotations.Parameters;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -37,18 +41,51 @@ public class BaseClass {
 
         logger = LogManager.getLogger(this.getClass());
 
-        switch (br.toLowerCase()) {
-            case "chrome":
-                dr = new ChromeDriver();
-                break;
+        if(p.getProperty("execution_env").equalsIgnoreCase("remote")){
 
-            case "edge":
-                dr = new EdgeDriver();
-                break;
+            DesiredCapabilities cap = new DesiredCapabilities();
 
-            default:
-                System.out.println("Invalid Browser name");
+            if(os.equalsIgnoreCase("windows")){
+                cap.setPlatform(Platform.WIN11);
+            }
+            else if (os.equalsIgnoreCase("mac")) {
+                cap.setPlatform(Platform.MAC);
+
+            } else if (os.equalsIgnoreCase("linux")) {
+                cap.setPlatform(Platform.LINUX);
+            }
+            else {
+                System.out.println("No Valid OS is Provided...");
                 return;
+            }
+
+            switch (br.toLowerCase()){
+                case "chrome" : cap.setBrowserName("chrome"); break;
+                case "edge" : cap.setBrowserName("Microsoft Edge"); break;
+                default:
+                    System.out.println("Provide valid Brwoser Name...");
+                    return;
+            }
+
+            dr = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+
+        }
+        if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+
+
+            switch (br.toLowerCase()) {
+                case "chrome":
+                    dr = new ChromeDriver();
+                    break;
+
+                case "edge":
+                    dr = new EdgeDriver();
+                    break;
+
+                default:
+                    System.out.println("Invalid Browser name");
+                    return;
+            }
         }
 
         dr.manage().deleteAllCookies();
